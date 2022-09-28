5e6c91b2333f4933046a0e0b3a14a919d178e25c
Jlphile <lkyz001@outlook.com>
2022-09-28 09:08:17 +0800

    30

--- 12_TypeScript其他内容补充/src/main.ts
+++ 12_TypeScript其他内容补充/src/main.ts
@@ -1,8 +1,28 @@
 import { add, sub } from './utils/math';
 import { time, price } from './utils/format';
+import axios from 'axios';
+import lodash from 'lodash';
+import nhltImage from './imgs/nhlt.jpg';
 
 console.log(add(20, 30));
 console.log(sub(30, 20));
 
 console.log(time.format('234234'));
 console.log(price.format(323));
+
+axios.get('http://123.207.32.32:8000/home/multidata').then((res) => {
+  console.log(res);
+});
+
+console.log(lodash.join(['abc', 'cba']));
+
+console.log(whyName);
+console.log(whyAge);
+console.log(whyHeight);
+
+whyFoo();
+
+const p = new Person('why', 18);
+console.log(p);
+
+$.ajax();
